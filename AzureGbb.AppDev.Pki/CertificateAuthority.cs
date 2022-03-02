1278346325cf19eb12c03835d05dee53a072cfa5
Ray Kao <ray@peopleandcode.com>
2022-03-02 14:49:52 -0500

    structure as classlib

---
+++ AzureGbb.AppDev.Pki/CertificateAuthority.cs
@@ -0,0 +1,16 @@
+﻿namespace AzureGbb.AppDev.Pki.Services;
+
+public abstract class CertificateAuthority
+{
+	public abstract String? GetRootCertificate();
+	public abstract void GenerateRootCa();
+	public abstract Task<byte[]> CreateRootCaInKeyVaultAsync();
+	public abstract Byte[] CreateRootCaInLocal();
+	public abstract void UploadRootCaToKeyVault(Byte[] certificate);
+	public void CreateRootCaInLocalAndUploadToKeyVault()
+	{
+		Byte[] certificate = CreateRootCaInLocal();
+		UploadRootCaToKeyVault(certificate);
+	}
+	public abstract Byte[]? SignContentWithRootCa(Byte[] content);
+}
