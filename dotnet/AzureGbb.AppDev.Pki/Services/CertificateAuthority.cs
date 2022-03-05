3000b4aaf12835c430c4b2377e8cdd2db23950fc
Ray Kao <ray@peopleandcode.com>
2022-03-04 21:03:51 -0500

    Update the interface for the async creation

--- dotnet/AzureGbb.AppDev.Pki/Services/CertificateAuthority.cs
+++ dotnet/AzureGbb.AppDev.Pki/Services/CertificateAuthority.cs
@@ -4,6 +4,6 @@ public abstract class CertificateAuthority
 {
 	protected abstract String? GetRootCertificate();
 	protected abstract void GenerateRootCa();
-	protected  abstract void CreateRootCaInKeyVault();
+	protected  abstract void CreateRootCaInKeyVaultAsync();
 	public abstract Byte[]? SignContentWithRootCa(Byte[] content);
 }
