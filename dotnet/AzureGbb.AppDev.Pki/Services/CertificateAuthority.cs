dfd142a3325ad650109eb1c4cea4b85dbf20fc18
Ray Kao <ray@peopleandcode.com>
2022-03-28 14:36:38 -0400

    refactor: cleaning up using statements

--- dotnet/AzureGbb.AppDev.Pki/Services/CertificateAuthority.cs
+++ dotnet/AzureGbb.AppDev.Pki/Services/CertificateAuthority.cs
@@ -1,6 +1,5 @@
 ﻿namespace AzureGBB.AppDev.Pki.Services;
 using System.Security.Cryptography.X509Certificates;
-using AzureGBB.AppDev.Pki.Models.Certificates;
 using AzureGBB.AppDev.Pki.Models.RSA;
 
 public abstract class CertificateAuthority
