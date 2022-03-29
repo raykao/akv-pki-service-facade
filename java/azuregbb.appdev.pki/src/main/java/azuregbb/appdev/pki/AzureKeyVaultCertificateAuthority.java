a7033eba804415e9f3bf12b61ebd39f8b6821e0e
Ray Kao <ray@peopleandcode.com>
2022-03-29 14:06:28 -0400

    feat: adding example for java

---
+++ java/azuregbb.appdev.pki/src/main/java/azuregbb/appdev/pki/AzureKeyVaultCertificateAuthority.java
@@ -0,0 +1,9 @@
+package azuregbb.appdev.pki;
+
+public class AzureKeyVaultCertificateAuthority
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ java/azuregbb.appdev.pki/src/test/java/azuregbb/appdev/pki/AzureKeyVaultCertificateAuthorityTest.java
@@ -0,0 +1,20 @@
+package azuregbb.appdev.pki;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AzureKeyVaultCertificateAuthorityTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
